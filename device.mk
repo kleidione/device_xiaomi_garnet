5834e62b3e2fc13f77b87b3282c3bd02ac24545e
kleidione Freitas <kleidione@gmail.com>
2024-03-14 18:27:55 -0300

    Garnet: Set stock firmware
    Ref:
    git@github.com:garnet-stuff/vendor_xiaomi_garnet-firmware.git
    
    Signed-off-by: kleidione <kleidione@gmail.com>

--- device.mk
+++ device.mk
@@ -213,6 +213,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# Firmware
+$(call inherit-product-if-exists, vendor/xiaomi/firmware/garnet/config.mk)
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
