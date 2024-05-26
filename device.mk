fb8cb55324b6e30217d2014fb4c46cb1668c859b
AdarshGrewal <adarshgrewal@gmail.com>
2024-05-26 20:56:20 +0530

    garnet: Setup overlays for XIG05
    Change-Id: Ia9ba10a73545683f8be43cf54739e231b0b51811

--- device.mk
+++ device.mk
@@ -374,12 +374,14 @@ PRODUCT_PACKAGES += \
     SettingsProviderOverlayGarnetPoco \
     SettingsProviderOverlayGarnetRedmi \
     SettingsProviderOverlayGarnetRedmiCN \
+    SettingsProviderOverlayGarnetXIG05 \
     SystemUIOverlayGarnet \
     TelephonyOverlayGarnet \
     WifiOverlayGarnet \
     WifiOverlayGarnetPoco \
     WifiOverlayGarnetRedmi \
-    WifiOverlayGarnetRedmiCN
+    WifiOverlayGarnetRedmiCN \
+    WifiOverlayGarnetXIG05
 
 # Power
 PRODUCT_PACKAGES += \
