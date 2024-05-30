1ace121a688f9fc379ebda407fc2c9ac513657c9
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-05-30 19:26:46 +0200

    garnet: Revert "Fix charger init trigger event after health AIDL move"
    https://github.com/LineageOS/android_system_core/blob/lineage-21.0/init/init.cpp#L1099
    
    This partially reverts commit 759c8743e4e85fbc3e010cad05d295decb6b8b3d.
    
    Change-Id: I284f80f364e9057a8373a015f0acaee3ef9e395d

--- extract-files.py
+++ extract-files.py
@@ -84,12 +84,6 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/etc/camera/pureShot_parameter.xml'
     ): blob_fixup()
         .regex_replace(r'=(\d+)>', r'="\1">'),
-    (
-        'vendor/etc/init/hw/init.batterysecret.rc',
-        'vendor/etc/init/hw/init.mi_thermald.rc',
-        'vendor/etc/init/hw/init.qti.kernel.rc'
-    ): blob_fixup()
-        .regex_replace('on charger', 'on property:init.svc.vendor.charger=running'),
     'vendor/etc/media_codecs_parrot_v0.xml': blob_fixup()
         .regex_replace('.+media_codecs_(google_audio|google_c2|google_telephony|vendor_audio).+\n', ''),
     'vendor/etc/vintf/manifest/c2_manifest_vendor.xml': blob_fixup()
