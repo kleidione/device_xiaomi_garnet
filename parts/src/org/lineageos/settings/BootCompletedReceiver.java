a23d4f6ceab404606036eefc6fcf02b4e4985bf7
kleidione Freitas <kleidione@gmail.com>
2024-07-09 22:21:52 -0300

    garnet: parts: Drop doze settings
    Signed-off-by: kleidione <kleidione@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,7 +22,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 
@@ -38,7 +37,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         }
 
         Log.i(TAG, "Boot completed, starting services");
-        DozeUtils.onBootCompleted(context);
         RefreshUtils.startService(context);
         ThermalUtils.startService(context);        
     }
