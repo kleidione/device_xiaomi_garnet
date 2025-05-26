b5fc6935dcd8f690fec94e2d162c06c62a8343c9
AdarshGrewal <adarshgrewal@gmail.com>
2025-05-26 17:44:30 +0530

    garnet: Move audio soong config to soong_config_set
    Change-Id: I13137e97ba383cc4f748ccb5c61053c848e2defa

--- device.mk
+++ device.mk
@@ -41,10 +41,7 @@ BOARD_SHIPPING_API_LEVEL := 31
 PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 
 # Audio
-SOONG_CONFIG_NAMESPACES += android_hardware_audio
-SOONG_CONFIG_android_hardware_audio += \
-    run_64bit
-SOONG_CONFIG_android_hardware_audio_run_64bit := true
+$(call soong_config_set, android_hardware_audio, run_64bit, true)
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
